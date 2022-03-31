6c542c5302e76d46e139f9cf883d14f5f1d65235
manuel-heav <manuelheav32@gmail.com>
2022-03-31 08:55:43 -0400

    got a basic express app

--- server/index.js
+++ server/index.js
@@ -2,7 +2,12 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 
-
+app.use(morgan('tiny'));
+app.use(cors());
 require('dotenv').config()
 
 const app = express();
+
+app.get('/videos', (req, res) => {
+    res.json([])
+});
