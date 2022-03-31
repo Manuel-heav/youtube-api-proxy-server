f3851b22bf29e481fb47a2b86778fa6b67df2f0b
manuel-heav <manuelheav32@gmail.com>
2022-03-31 09:00:43 -0400

    set up notfound and errorHandler

--- server/index.js
+++ server/index.js
@@ -2,12 +2,37 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 
+const app = express();
 app.use(morgan('tiny'));
 app.use(cors());
 require('dotenv').config()
 
-const app = express();
 
 app.get('/videos', (req, res) => {
     res.json([])
 });
+
+
+const notFound = (req, res, next) =>  {
+    res.status(404)
+    const error = new Error('Not Found')
+    next(error)
+}
+
+const errorHandler= (error, req, res, next) => {
+    res.status(res.statusCode || 500)
+    res.json({
+        message: error.message
+    })
+}
+
+app.use(notFound)
+app.use(errorHandler)
+
+
+const port = process.env.PORT || 5000
+
+
+app.listen(port, () => {
+    console.log(`Listening On port ${port}`)
+})
